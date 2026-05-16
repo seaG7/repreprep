9699ba77bc7ce12916c155a049b57c535d32de4e
seaG7 <neyro.yt@gmail.com>
2026-05-16 08:27:22 +0300

    google services fix

--- MireaProject/app/src/main/java/com/mirea/Samsonova/mireaproject/LoginActivity.java
+++ MireaProject/app/src/main/java/com/mirea/Samsonova/mireaproject/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -28,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
